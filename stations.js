d7bdf0c8260d7ab1e738b6f83d3c0ee0aff2283d
Eren <95621105+ErenS61@users.noreply.github.com>
2025-06-08 00:08:07 +0200

    Update stations.js

--- stations.js
+++ stations.js
@@ -1,6 +1,8 @@
 // Liste des stations disponibles
 const stations = [
     { id: "68180001", nom: "Hesso Horbourg-Wihr" },
+
+    { id: "68000006", nom: "Wintzenheim" },
     
     { id: "68400001", nom: "Super U Riedisheim" },
     
